816bfac0d816e9f5acd0586a502240b74e35ea19
zardos <callummclennan1@googlemail.com>
2022-01-17 18:51:38 +0000

    a bit more practice on neural nets

--- 02-imbd.py
+++ 02-imbd.py
@@ -1,5 +1,10 @@
 import numpy as np
+import plotly
+import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import imdb
+from tensorflow.keras import optimizers
+from tensorflow.keras import losses
+from tensorflow.keras import metrics
 from tensorflow.keras import models
 from tensorflow.keras import layers
 
@@ -33,8 +38,17 @@ def print_dataset_related_info(train_data, train_labels):
     # decode_review(train_data[0])
     pass
 
+def plot_accuracy(loss_values, val_loss_values):
+    epochs = range(1, 21)
+    plt.plot(epochs, loss_values, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
 
-def main():
+def load_data():
     # Load in training/test datasets
     (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = 10000)
     
@@ -43,14 +57,70 @@ def main():
     x_test = vectorize_sequences(test_data)
 
     # Vectorise training and test labels
-    y_train = vectorize_sequences(train_labels).astype('float32')
-    y_test = vectorize_sequences(test_labels).astype('float32')
+    y_train = np.asarray(train_labels).astype('float32')
+    y_test = np.asarray(test_labels).astype('float32')
 
+    return x_train, y_train, x_test, y_test
+
+
+def trial_1():
+    """Breaks training data to train/test datasets
+    Trains a network model on the training dataset and validates using test dataset
+    plot accuracy of network - 
+    """
+    x_train, y_train, x_test, y_test = load_data()
     # Prepare network
     network = models.Sequential()
     network.add(layers.Dense(16, activation='relu', input_shape = (10000,)))
     network.add(layers.Dense(16, activation='relu'))
-    network.add(1, activation = 'sigmoid')
+    network.add(layers.Dense(1, activation = 'sigmoid'))
+
+    x_val = x_train[:10000]
+    partial_x_train = x_train[10000:]
+    y_val = y_train[:10000]
+    partial_y_train = y_train[10000:]
     
+    # Compile the neural network / model
+    network.compile(optimizer='rmsprop',
+                loss='binary_crossentropy',
+                metrics=['acc'])
+
+    history = network.fit(partial_x_train,
+                        partial_y_train,
+                        epochs=20,
+                        batch_size=512,
+                        validation_data=(x_val, y_val))
+    
+    history_dict = history.history
+    print(history_dict.keys())
+    plot_accuracy(history_dict['loss'], history_dict['val_loss'])
+
+def trial_2():
+    """Creates basic neural network model
+    Trains model over less epochs (iterations) with a larger batch size
+    Evaluates trained model using test set - 88% accuracy
+    """
+    x_train, y_train, x_test, y_test = load_data()
+    model = models.Sequential()
+    # Input layer
+    model.add(layers.Dense(32, activation = 'tanh', input_shape = (10000,)))
+    # Hidden layer
+    model.add(layers.Dense(64, activation = 'tanh'))
+    # Output layer
+    model.add(layers.Dense(1, activation = 'sigmoid'))
+    model.compile(optimizer='rmsprop',
+                loss='mse',
+                metrics=['accuracy'])
+                
+    model.fit(x_train, y_train, epochs=4, batch_size = 512)
+    results = model.evaluate(x_test, y_test)
+    
+
+
+def main():
+    # trial_1()
+    trial_2()
+
+
 if __name__ == "__main__":
     main()
---
+++ 04-bottleneck-network.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import reuters
+from keras import models
+from keras import layers
+from keras.utils.np_utils import to_categorical
+
+def plot_loss(loss_values, val_loss_values):
+    epochs = range(1, 21)
+    plt.plot(epochs, loss_values, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+
+def plot_accuracy(acc, val_acc):
+    epochs = range(1, 21)
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+
+def to_one_hot(labels, dimension = 46):
+    """Embeds each label with all zero vector with a 1 in the place of label index
+
+    Returns:
+        [type]: [description]
+    """
+    results = np.zeros((len(labels), dimension))
+    for i, label in enumerate(labels):
+        results[i, label] = 1
+    return results
+
+
+def vectorize_sequences(sequences, dimension = 10000):
+    """Encodes integer representations of reviews into a binary matrix
+
+    Args:
+        sequences (list): Integer representation of review
+        dimension (int, optional): . Defaults to 10000.
+
+    Returns:
+        [type]: [description]
+    """
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+
+def main():
+    # Load Reuters Dataset
+    (train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words = 10000)
+    # one_hot_train_labels = to_one_hot(train_labels)
+    # one_hot_test_labels = to_one_hot(test_labels)
+
+    x_train = vectorize_sequences(train_data)
+    x_test = vectorize_sequences(test_data)
+
+    one_hot_train_labels = to_categorical(train_labels)
+    one_hot_test_labels = to_categorical(test_labels)
+
+    model = models.Sequential()
+    # Input layer
+    model.add(layers.Dense(64, activation = 'relu', input_shape=(10000,)))
+    # Hidden layer
+    model.add(layers.Dense(4, activation = 'relu'))
+    # Output layer (46 outputs)
+    model.add(layers.Dense(46, activation = 'softmax'))
+
+    model.compile(optimizer = 'rmsprop',
+                    loss = 'categorical_crossentropy',
+                    metrics = ['accuracy'])
+
+    x_val = x_train[:1000]
+    partial_x_train = x_train[1000:]
+    y_val = one_hot_train_labels[:1000]
+    partial_y_train = one_hot_train_labels[1000:]
+    # Adjusted to 9 epochs
+    history = model.fit(partial_x_train,
+                        partial_y_train,
+                        epochs = 20,
+                        batch_size = 128,
+                        validation_data  = (x_val, y_val))
+    
+    results = model.evaluate(x_test, one_hot_test_labels)
+    print(history.history.keys())
+    plot_accuracy(history.history['accuracy'], history.history['val_accuracy'])
+    plot_loss(history.history['loss'], history.history['val_loss'])
+
+    predictions = model.predict(x_test)
+    print(np.argmax(predictions[0]))
+    
+
+
+if __name__ == "__main__":
+    main()
