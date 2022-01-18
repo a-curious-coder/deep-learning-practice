81dc54d146af37f78beb3fc36a711f2cdde5b6b5
zardoss <callummclennan1@googlemail.com>
2022-01-18 11:31:19 +0000

    Regression Problem

--- 04-bottleneck-network.py
+++ 04-bottleneck-network.py
@@ -1,31 +1,10 @@
+# Import file in local working dir for plotting graphs
+import plot_graphs as pg
 import numpy as np
-import matplotlib.pyplot as plt
 from keras.datasets import reuters
 from keras import models
 from keras import layers
 from keras.utils.np_utils import to_categorical
-
-def plot_loss(loss_values, val_loss_values):
-    epochs = range(1, 21)
-    plt.plot(epochs, loss_values, 'bo', label='Training loss')
-    plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show()
-
-
-def plot_accuracy(acc, val_acc):
-    epochs = range(1, 21)
-    plt.plot(epochs, acc, 'bo', label='Training acc')
-    plt.plot(epochs, val_acc, 'b', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show()
-
 
 def to_one_hot(labels, dimension = 46):
     """Embeds each label with all zero vector with a 1 in the place of label index
@@ -56,6 +35,8 @@ def vectorize_sequences(sequences, dimension = 10000):
 
 
 def main():
+    pg.verify_import()
+    epochs = 20
     # Load Reuters Dataset
     (train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words = 10000)
     # one_hot_train_labels = to_one_hot(train_labels)
@@ -64,40 +45,52 @@ def main():
     x_train = vectorize_sequences(train_data)
     x_test = vectorize_sequences(test_data)
 
+    y_train = np.array(train_labels)
+    y_test= np.array(test_labels)
+
     one_hot_train_labels = to_categorical(train_labels)
     one_hot_test_labels = to_categorical(test_labels)
 
+    # Initialise Network
     model = models.Sequential()
     # Input layer
     model.add(layers.Dense(64, activation = 'relu', input_shape=(10000,)))
     # Hidden layer
-    model.add(layers.Dense(4, activation = 'relu'))
+    model.add(layers.Dense(32, activation = 'relu'))
     # Output layer (46 outputs)
     model.add(layers.Dense(46, activation = 'softmax'))
 
     model.compile(optimizer = 'rmsprop',
-                    loss = 'categorical_crossentropy',
+                    loss = 'sparse_categorical_crossentropy',
                     metrics = ['accuracy'])
 
     x_val = x_train[:1000]
     partial_x_train = x_train[1000:]
-    y_val = one_hot_train_labels[:1000]
-    partial_y_train = one_hot_train_labels[1000:]
+    # y_val = one_hot_train_labels[:1000]
+    # partial_y_train = one_hot_train_labels[1000:]
+    y_val = np.array(y_train[:1000])
+    partial_y_train = np.array(y_train[1000:])
+    
+    results = model.evaluate(x_test, y_test)
+
     # Adjusted to 9 epochs
     history = model.fit(partial_x_train,
                         partial_y_train,
-                        epochs = 20,
+                        epochs = epochs,
                         batch_size = 128,
                         validation_data  = (x_val, y_val))
-    
-    results = model.evaluate(x_test, one_hot_test_labels)
-    print(history.history.keys())
-    plot_accuracy(history.history['accuracy'], history.history['val_accuracy'])
-    plot_loss(history.history['loss'], history.history['val_loss'])
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    # pg.plot_accuracy(history.history['accuracy'], history.history['val_accuracy'])
+    # pg.plot_loss(history.history['loss'], history.history['val_loss'])
+    # pg.plot_all_graphs(loss, val_loss, acc, val_acc)
 
     predictions = model.predict(x_test)
-    print(np.argmax(predictions[0]))
-    
+
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Deep Learning with Python
 Just following the exercises in the book :P
+
+Files with prefix ranging 01-04 are classification problems for discrete labels.
+## 05 Boston Housing (Regression)
+Predicts the continuous value 
+Scalar Regression - A regression where you're trying to predict a single continuous value
